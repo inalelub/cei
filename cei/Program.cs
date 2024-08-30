3e76ea4acf3a5a4d7dae83c22796ba2ddda4783a
Bulelani Botman <69175349+inalelub@users.noreply.github.com>
2024-08-30 14:04:44 +0200

    feat: configured all database necessities
    Downloaded nuget packages for EF Core for database connections & provided a connection string to the
    database

--- cei/Program.cs
+++ cei/Program.cs
@@ -1,7 +1,11 @@
+using cei.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(optionsAction: options => options.UseSqlServer("LocalConnection"));
 
 var app = builder.Build();
 
